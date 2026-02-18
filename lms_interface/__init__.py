87c526a8b2d3213453a2b921380d71a914b2a8c8
samogden <samuel.s.ogden@gmail.com>
2026-02-17 17:14:37 -0800

    Adding in a new lms_interface update -- note, we might have bugs on main right now

---
+++ lms_interface/__init__.py
@@ -0,0 +1,23 @@
+"""
+LMS integration for GradingWebUI
+
+Vendored from LMSInterface v0.2.2 (2026-02-08)
+"""
+
+__version__ = "0.2.2"
+__vendored_from__ = "LMSInterface"
+__vendored_date__ = "2026-02-08"
+
+try:
+  from importlib.metadata import PackageNotFoundError, version
+except Exception:  # pragma: no cover - fallback for older Python
+  PackageNotFoundError = Exception  # type: ignore
+  version = None  # type: ignore
+
+if "__version__" not in globals():
+  try:
+    if version is None:
+      raise PackageNotFoundError
+    __version__ = version("lms-interface")
+  except PackageNotFoundError:
+    __version__ = "vendored"
